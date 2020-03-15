4b244107ed3cff46cfdda53ba0f4271e36ec754b
Erufen <58253085+Erufen@users.noreply.github.com>
2020-03-15 17:36:47 +0100

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,24 @@
+/*
+Projet POO : LesMinimesEnUnClic
+
+@A rendre le 31 mars 2020 (au plus tard)
+
+PROGRAMME PRINCIPAL
+
+*/
+
+#include <iostream>
+#include "Bateau.h"
+
+using namespace std;
+
+int main(){
+    
+
+
+
+
+
+
+    return 0;
+}
