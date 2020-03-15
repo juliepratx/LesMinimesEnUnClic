2caf0dfbfe688724c707e31e4b85a3f8105af1cb
Erufen <58253085+Erufen@users.noreply.github.com>
2020-03-15 17:36:55 +0100

    Create Bateau.h

---
+++ Bateau.h
@@ -0,0 +1,27 @@
+/*
+
+Ceci est la classe bateau.
+
+*/
+
+
+#ifndef BATEAU_H_INCLUDED
+#define BATEAU_H_INCLUDED
+
+
+class Bateau{
+
+
+
+public:
+
+	Bateau(); // constructeur par défaut
+	~Bateau(); // destructeur
+
+private:
+
+};
+
+
+
+#endif
