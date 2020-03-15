4e84a8cf82c59ea383bd353943b22bd8d2a466b4
Erufen <58253085+Erufen@users.noreply.github.com>
2020-03-15 17:49:27 +0100

    Create GestionDuPort.h

---
+++ GestionDuPort.h
@@ -0,0 +1,33 @@
+/*
+
+Ceci est la classe gestion du port.
+
+Elle sert à enregistrer les données saisies par l'utilisateur.
+
+*/
+
+
+#ifndef GESTIONDUPORT_H_INCLUDED
+#define GESTIONDUPORT_H_INCLUDED
+
+
+
+class GestionDuPort{
+
+
+
+public:
+	GestionDuPort();
+	~GestionDuPort();
+
+private:
+
+};
+
+
+
+
+
+
+
+#endif
