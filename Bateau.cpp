0859b7233362189b24725245e2a6ca7aa4b4a850
Erufen <58253085+Erufen@users.noreply.github.com>
2020-03-20 15:55:40 +0100

    Update Bateau.cpp

--- Bateau.cpp
+++ Bateau.cpp
@@ -10,9 +10,20 @@
 using namespace std;
 
 
-
-
 Bateau::Bateau() {
+
+	taille = 0;
+	tailleMaxi = 0;
+	tailleMini = 0;
+	nombreDeCabine = 0;
+	electricite[0] = '\0';
+	eau[0] = '\0';
+
+}
+
+Bateau::Bateau(int taille){
+
+	cout << "Le bateau a une longueur de : " << taille << "mètres" << endl;
 
 }
 
