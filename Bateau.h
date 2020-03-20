bc6c05cdff3435277d65e34a0528fd1778f86230
Erufen <58253085+Erufen@users.noreply.github.com>
2020-03-20 15:55:46 +0100

    Update Bateau.h

--- Bateau.h
+++ Bateau.h
@@ -11,14 +11,22 @@ Ceci est la classe bateau.
 
 class Bateau{
 
-
-
 public:
 
 	Bateau(); // constructeur par défaut
+	Bateau(int taille); // indiquer la taille
 	~Bateau(); // destructeur
 
 private:
+
+	int taille;
+	int tailleMini = 0;
+	int tailleMaxi;
+	int nombreDeCabine;
+	char electricite[3] = {'O', 'U', 'I'}; // oui par défaut
+	char eau[3] = {'O', 'U', 'I'}; // idem
+
+
 
 };
 
