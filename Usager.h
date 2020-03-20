3d55b671b954bce9b3b6876d409006f3e0241e4e
Erufen <58253085+Erufen@users.noreply.github.com>
2020-03-20 15:55:57 +0100

    Create Usager.h

---
+++ Usager.h
@@ -0,0 +1,28 @@
+#ifndef USAGER_H_INCLUDED
+#define USAGER_H_INCLUDED
+
+
+class Usager{
+
+public:
+
+	Usager();
+	~Usager();
+
+
+	void abonnes();
+	void passagers();
+
+private:
+
+
+
+
+
+};
+
+
+
+
+
+#endif
