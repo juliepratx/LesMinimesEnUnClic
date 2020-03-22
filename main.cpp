22bad2b197155ee1f3c98366ba296004912cc036
Erufen <58253085+Erufen@users.noreply.github.com>
2020-03-22 17:47:39 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 /*
 Projet POO : LesMinimesEnUnClic
 
+Le port de plaisance de La Rochelle est composé de :
+- 5'000 places à flot
+- (dont) 300 places visiteurs
+- 30 corps morts
+
 @A rendre le 31 mars 2020 (au plus tard)
 
 PROGRAMME PRINCIPAL
@@ -9,6 +14,12 @@ PROGRAMME PRINCIPAL
 
 #include <iostream>
 #include "Bateau.h"
+#include "GestionDuPort.h"
+#include "Usager.h"
+#include "VoilierNonHabitable.h"
+#include "VoilierType1.h"
+#include "VoilierType2.h"
+#include "Tarifs.h"
 
 using namespace std;
 
