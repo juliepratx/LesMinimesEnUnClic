d867b9036242c15a2a00c3ff542c05ecd13ba316
Erufen <58253085+Erufen@users.noreply.github.com>
2020-03-22 17:47:32 +0100

    Update Bateau.cpp

--- Bateau.cpp
+++ Bateau.cpp
@@ -1,6 +1,8 @@
 /*
-
-
+Classe qui regroupe toutes les caractéristiques des bateaux :
+- la taille (mini et maxi)
+- le nombre de cabine
+- le racordement à l'eau et à l'électricité
 */
 
 #include <iostream>
@@ -13,8 +15,8 @@ using namespace std;
 Bateau::Bateau() {
 
 	taille = 0;
-	tailleMaxi = 0;
 	tailleMini = 0;
+	tailleMaxi = 0;
 	nombreDeCabine = 0;
 	electricite[0] = '\0';
 	eau[0] = '\0';
@@ -25,7 +27,25 @@ Bateau::Bateau(int taille){
 
 	cout << "Le bateau a une longueur de : " << taille << "mètres" << endl;
 
+	if (taille < 10) {
+		cout << "Type de bâteau : Voilier non habitable" << endl;
+		cout << "Branchement à l'eau et à l'électricté non disponible." << endl;
+	}
+
+	if (taille >= 10 && taille < 25) {
+		cout << "Type de bâteau : Voilier de type 1" << endl;
+		cout << "Branchement à l'eau et à l'électricité en supplément." << endl;
+	}
+
+	else {
+		cout << "Type de bâteau : Voilier de type 2" << endl;
+		cout << "Places disponibles limitées !" << endl;
+		cout << "Branchement à l'eau et à l'électricité en supplément." << endl;
+	}
+
 }
+
+
 
 Bateau::~Bateau() { ; }
 
