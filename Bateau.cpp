c6f2f3e88a5ed6c768ed86674707420d5057d8b7
Erufen <58253085+Erufen@users.noreply.github.com>
2020-03-24 19:39:36 +0100

    Update Bateau.cpp

--- Bateau.cpp
+++ Bateau.cpp
@@ -7,6 +7,7 @@ Classe qui regroupe toutes les caractéristiques des bateaux :
 
 #include <iostream>
 #include "Bateau.h"
+#include <string>
 
 
 using namespace std;
@@ -14,6 +15,7 @@ using namespace std;
 
 Bateau::Bateau() {
 
+	typeBateau = "NULL";
 	taille = 0;
 	tailleMini = 0;
 	tailleMaxi = 0;
@@ -24,22 +26,27 @@ Bateau::Bateau() {
 }
 
 Bateau::Bateau(int taille){
-
-	cout << "Le bateau a une longueur de : " << taille << "mètres" << endl;
+	
 
 	if (taille < 10) {
+		cout << endl;
+		cout << "Taille du bâteau : " << taille << " mètres" << endl;
 		cout << "Type de bâteau : Voilier non habitable" << endl;
 		cout << "Branchement à l'eau et à l'électricté non disponible." << endl;
 	}
 
 	if (taille >= 10 && taille < 25) {
+		cout << endl;
+		cout << "Taille du bâteau : " << taille << " mètres" << endl;
 		cout << "Type de bâteau : Voilier de type 1" << endl;
 		cout << "Branchement à l'eau et à l'électricité en supplément." << endl;
 	}
 
-	else {
+	if (taille > 25) {
+		cout << endl;
+		cout << "Taille du bâteau : " << taille << " mètres" << endl;
 		cout << "Type de bâteau : Voilier de type 2" << endl;
-		cout << "Places disponibles limitées !" << endl;
+		cout << "Attention : places disponibles limitées !" << endl;
 		cout << "Branchement à l'eau et à l'électricité en supplément." << endl;
 	}
 
@@ -47,5 +54,84 @@ Bateau::Bateau(int taille){
 
 
 
-Bateau::~Bateau() { ; }
 
+int Bateau::nbDePlaceDispo(string typeDeBateau, string typeDeVisiteur, bool corpsMort)
+{
+	int placesAFlot = 4'700;
+	int placesVisiteur = 300;
+	int placesCorpsMort = 30;
+
+	if (typeDeBateau == "Voilier non habitable" && typeDeVisiteur == "abonné" && corpsMort == false) {
+		placesAFlot--;
+		cout << "Type d'emplacement : place à flot" << endl;
+		cout << "Statut : abonné" << endl;
+		cout << "Il reste maintenant " << placesAFlot << " places à flot disponibles !" << endl;
+		if (placesAFlot == 0) {
+			cout << "Aucune place disponible !" << endl;
+		}
+	}
+	if (typeDeBateau == "Voilier non habitable" && typeDeVisiteur == "passager" && corpsMort == false) {
+		placesVisiteur--;
+		cout << "Type d'emplacement : une place visiteur." << endl;
+		cout << "Statut : passager" << endl;
+		cout << "Il reste maintenant " << placesVisiteur << " places visiteurs disponibles !" << endl;
+		if (placesVisiteur == 0) {
+			cout << "Aucune place disponible !" << endl;
+		}
+	}
+	if (typeDeBateau == "Voilier type 1" && typeDeVisiteur == "abonné" && corpsMort == false) {
+		placesAFlot--;
+		cout << "Type d'emplacement : sur une place à flot." << endl;
+		cout << "Statut : abonné" << endl;
+		cout << "Il reste maintenant " << placesAFlot << " places disponibles !" << endl;
+		if (placesAFlot == 0) {
+			cout << "Aucune place disponible !" << endl;
+		}
+	}
+	if (typeDeBateau == "Voilier type 1" && typeDeVisiteur == "passager" && corpsMort == false) {
+		placesVisiteur--;
+		cout << "Type d'emplacement : sur une place visiteur." << endl;
+		cout << "Statut : passager" << endl;
+		cout << "Il reste maintenant " << placesVisiteur << " places visiteurs disponibles !" << endl;
+		if (placesVisiteur == 0) {
+			cout << "Aucune place disponible !" << endl;
+		}
+	}
+	if (typeDeBateau == "Voilier type 2" && typeDeVisiteur == "abonné" && corpsMort == false) {
+		placesAFlot--;
+		cout << "Type d'emplacement : sur une place à flot." << endl;
+		cout << "Statut : abonné" << endl;
+		cout << "Il reste maintenant " << placesAFlot << " places disponibles !" << endl;
+		if (placesAFlot == 0) {
+			cout << "Aucune place disponible !" << endl;
+		}
+	}
+	if (typeDeBateau == "Voilier type 2" && typeDeVisiteur == "passager" && corpsMort == false) {
+		placesVisiteur--;
+		cout << "Type d'emplacement : sur une place visiteur." << endl;
+		cout << "Statut : passager" << endl;
+		cout << "Il reste maintenant " << placesVisiteur << " places visiteurs disponibles !" << endl;
+		if (placesVisiteur == 0) {
+			cout << "Aucune place disponible !" << endl;
+		}
+	}
+	if (corpsMort == true) {
+		placesCorpsMort--;
+		cout << "Type d'emplacement : sur un corps mort." << endl;
+		cout << "Il reste maintenant " << placesCorpsMort << " corps mort disponibles !" << endl;
+		if (placesCorpsMort == 0) {
+			cout << "Aucun corps disponible !" << endl;
+		}
+	}
+
+	return placesAFlot, placesVisiteur, placesCorpsMort;
+}
+
+void Bateau::afficheBateau()
+{
+	cout << "Type de bâteau : " << typeBateau << endl;
+}
+
+
+
+Bateau::~Bateau() { ; }
