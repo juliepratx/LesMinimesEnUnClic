98147447b11d54a18ed0107d6259de35b27dfb12
Erufen <58253085+Erufen@users.noreply.github.com>
2020-03-25 15:27:31 +0100

    Update Usager.h

--- Usager.h
+++ Usager.h
@@ -1,12 +1,17 @@
 #ifndef USAGER_H_INCLUDED
 #define USAGER_H_INCLUDED
 
+#include <iostream>
+#include <string>
+
+using namespace std;
 
 class Usager{
 
 public:
 
 	Usager();
+	Usager(string typeDeBateau);
 	~Usager();
 
 
@@ -15,7 +20,12 @@ public:
 
 private:
 
-
+	string idVNH ="VNH";
+	int id_VNH = 0;
+	string idVT1 = "VT1";
+	int id_VT1 = 0;
+	string idVT2 = "VT2";
+	int id_VT2 = 0;
 
 };
 
