5acd9cc01ab4074b78f43ea62c9b8bde213c81bb
Erufen <58253085+Erufen@users.noreply.github.com>
2020-03-25 15:27:28 +0100

    Update Usager.cpp

--- Usager.cpp
+++ Usager.cpp
@@ -4,22 +4,54 @@ Classe des usagers du Port
 
 #include "Usager.h"
 #include <iostream>
+#include <string>
 
 
 using namespace std;
 
-Usager::Usager()
-{
+Usager::Usager(){
+
+	idVNH = "VNH";
+	id_VNH = '\0';
+	idVT1 = "VT1";
+	id_VT1 = '\0';
+	idVT2 = "VT2";
+	id_VT2 = '\0';
+
 }
 
-Usager::~Usager(){;}
+Usager::Usager(string typeDeBateau)
+{
+	string idVNH = "VNH"; string idVT1 = "VT1"; string idVT2 = "VT2";
+	int id_VNH = 0; int id_VT1 = 0; int id_VT2 = 0;
+
+	if (typeDeBateau == "Voilier non habitable") {
+		id_VNH++;
+		cout << "Identifiant : " << idVNH << " " << id_VNH << endl;
+	}
+	
+	if (typeDeBateau == "Voilier type 1") {
+		id_VT2++;
+		cout << "Identifiant : " << idVT1 << " " << id_VT1 << endl;
+	}
+	
+	if (typeDeBateau == "Voilier type 1") {
+		id_VT2++;
+		cout << "Identifiant : " << idVT2 << " " << id_VT2 << endl;
+	}
+
+}
+
+
 
 void Usager::abonnes(){
 
 	cout << "Je suis un abonné !" << endl;
+
 }
 
 void Usager::passagers(){
 
 	cout << "Je suis de passage !" << endl;
 }
+Usager::~Usager() { ; }
