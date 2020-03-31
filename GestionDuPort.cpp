6f592124e61ce5c234c8ec470a970f588e7ea606
Erufen <58253085+Erufen@users.noreply.github.com>
2020-03-31 14:42:33 +0200

    Update GestionDuPort.cpp

--- GestionDuPort.cpp
+++ GestionDuPort.cpp
@@ -8,12 +8,25 @@ using namespace std;
 
 GestionDuPort::GestionDuPort(){
 
-
-
-
 }
 
 GestionDuPort::~GestionDuPort() {
 	;
+}
+
+void GestionDuPort::afficheListeClient()
+{
+
+	cout << "Liste des clients :" << endl;
+
+
+}
+
+void GestionDuPort::afficheListePlacesDispo()
+{
+	cout << "Liste des places disponibles : " << endl;
+	cout << "Places à flot : " << nbDePlaceAFlot << endl;
+	cout << "Places visiteurs : " << nbDePlaceVisiteurs << endl;
+	cout << "Corps mort : " << ndBdePlaceCorpsMort << endl;
 }
 
