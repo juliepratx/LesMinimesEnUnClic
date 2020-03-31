4416e2cafec32d644d12fd55f1309834456589e4
Erufen <58253085+Erufen@users.noreply.github.com>
2020-03-31 15:56:39 +0200

    Update Usager.h

--- Usager.h
+++ Usager.h
@@ -14,20 +14,23 @@ public:
 //	Usager(const Usager& unUsager, string typeDeBateau);
 	~Usager();
 
-//	void afficheID(string typeDeBateau);
+	void afficheID(string typeDeBateau);
 	void abonnes();
 	void passagers();
-	void id();
+	void id(string typeDeBateau);
 
 private:
 
+	string idVNH = "VNH", idVT1 = "VT1", idVT2 = "VT2";
+	int id_VNH = 0; int id_VT1 = 0; int id_VT2 = 0;
 
+	/*
 	// Modèle d'identifiant :
 
 	char idVNH[7] = {'V', 'N', 'H', '0', '0', '0', '0'};
 	char idVT1[7] = {'V', 'T', '1', '0', '0', '0', '0'};
 	char idVT2[7] = {'V', 'T', '2', '0', '0', '0', '0'};
-	
+	*/
 
 };
 
